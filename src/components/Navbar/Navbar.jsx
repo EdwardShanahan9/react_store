112c601afe4870ba52363a4155e254831289b8d6
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-08-13 18:35:28 +0100

    Style Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,37 @@
+import { FaSearch, FaUser } from "react-icons/fa";
+
 const Navbar = () => {
   return (
-    <nav>
-      <a href="">React_Store</a>
+    <nav className="navbar">
+      <a className="navbar_logo" href="">
+        React_Store
+      </a>
+
+      <ul className="navbar__list">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Categories</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+      </ul>
+
+      <div className="navbar__right">
+        <div className="navbar__search">
+          <input type="text" />
+          <FaSearch
+            className="navbar__search-icon"
+            style={{ marginLeft: "8px" }}
+          />
+        </div>
+        <FaUser
+          className="navbar__account-icon"
+          style={{ marginLeft: "16px" }}
+        />
+      </div>
     </nav>
   );
 };
