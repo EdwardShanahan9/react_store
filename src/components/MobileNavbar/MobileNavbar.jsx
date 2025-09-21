7b87f425aa3fdc264a1574bc65b83d3a78937972
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-09-21 10:21:47 +0100

    Style MobileNavbar list

--- src/components/MobileNavbar/MobileNavbar.jsx
+++ src/components/MobileNavbar/MobileNavbar.jsx
@@ -2,7 +2,7 @@ import "./MobileNavbar.css";
 
 const MobileNavbar = ({ navLinks }) => {
   return (
-    <ul className="navbar__mobile-list">
+    <ul className="navbar__mobile-list active">
       {navLinks.map((link, index) => (
         <li key={index}>
           <a href={link.path}>{link.name}</a>
